a998d8d376bac32d1ecd7f99ebcd27a14e9cb828
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-05-23 17:59:09 +0530

    Some changes

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -72,23 +72,26 @@
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
-const FoodItem=({data})=>{
-    console.log(data);
-    let navigate = useNavigate();
-    return(
-        <>  
-            {
-                (!data) ?"Not Found": data.map(item=>{
-                    return(
-                    <div className="card" key={item.idMeal} onClick={()=>navigate(`/${item.idMeal}`)}>
-                        <img src={item.strMealThumb} alt="" />
-                        <h3>{item.strMeal}</h3>
-                    </div>
-                    )
-                })
-            }
-            
-        </>
-    )
-}
+const FoodItem = ({ data }) => {
+  console.log(data);
+  let navigate = useNavigate();
+  return (
+    <>
+      {!data
+        ? "Not Found"
+        : data.map((item) => {
+            return (
+              <div
+                className="card"
+                key={item.idMeal}
+                //  onClick={()=>navigate(`/${item.idMeal}`)}
+              >
+                <img src={item.strMealThumb} alt="" />
+                <h3>{item.strMeal}</h3>
+              </div>
+            );
+          })}
+    </>
+  );
+};
 export default FoodItem;
